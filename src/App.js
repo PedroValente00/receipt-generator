b35aaf7f95876c69672afb00e902245d6d4199d5
unknown <pedrovalente_00@hotmail.com>
2023-12-07 09:24:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import logo from "./shell.bmp"
+const { jsPDF } = require("jspdf")
+const doc = new jsPDF();
 
 function App() {
+  const [bla, setBla] = useState("");
+
+  // const handleChange = (e) =>{
+  //   setBla(e.target.value)
+  // }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    // console.log("the name is "+e.target.name.value)
+    doc.text(`Name: ${e.target[0].value}`,10,30);
+    doc.text(`Price: ${e.target[1].value}`,10,40);
+    doc.text(`Date: ${e.target[2].value}`,10,50);
+    doc.addImage(logo, "JPEG", 150, 10, 40, 40);
+    doc.save(`${e.target.name.value}.pdf`);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Receipt generator</p>        
+      <form onSubmit={handleSubmit}>
+        <p><label htmlFor="name">Name of the customer: </label>
+        <input type="text" name="name" id="name"/> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p><label htmlFor="price">Amount paid in GBP: </label>
+        <input type="text" name="price" id="price"  /> 
+        </p>
+        <p><label htmlFor="date">Date: </label>
+        <input type="text" name="date" id="date" /> 
+        </p>
+        <button>Get receipt</button>
+</form>
     </div>
   );
 }
